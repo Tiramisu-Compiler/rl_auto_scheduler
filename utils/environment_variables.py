ca78134b914f4ac3b2f3ebaabbb2549cef7da60a
hbenyamina <gh_benyamina@esi.dz>
2022-09-27 06:25:10 -0400

    removed a hardcoded path

--- utils/environment_variables.py
+++ utils/environment_variables.py
@@ -7,4 +7,4 @@ def configure_env_variables(config):
     # The two environment variables below are set to 1 to avoid a Docker container error
     os.environ["RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE"] = "1"
     os.environ["RAY_ALLOW_SLOW_STORAGE"] = "1"
-    os.environ['TUNE_RESULT_DIR'] = "/data/scratch/hbenyamina/rl_auto_scheduler" #export TUNE_RESULT_DIR=/data/scratch/hbenyamina/rl_auto_scheduler
+    os.environ['TUNE_RESULT_DIR'] = os.getcwd()
