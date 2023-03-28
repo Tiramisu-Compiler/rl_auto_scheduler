cfc4024dac755e546f118107f88cde9a75d8c699
Smail KOURTA <sk10691@nyu.edu>
2023-03-28 04:30:48 -0400

    added seed, comments and types and reverted tiramisu config

--- utils/dataset_utilities.py
+++ utils/dataset_utilities.py
@@ -3,12 +3,15 @@ import json
 import os
 import pickle
 import random
+from typing import Tuple
 import numpy as np
 import ray
 
+# Frequency at which the dataset is saved to disk
 SAVING_FREQUENCY = 10000
 
 
+# Enum for the dataset format
 class DataSetFormat:
     PICKLE = "PICKLE"
     JSON = "JSON"
@@ -17,13 +20,31 @@ class DataSetFormat:
 
 @ray.remote
 class DatasetAgent:
+    """
+    DatasetAgent is a class that is used to read the dataset and update it.
+    It is used to read the dataset from disk and update it with the new functions.
+    It is also used to save the dataset to disk.
+
+    There are currently two modes of operation:
+    1. use_dataset = True: In this mode, the dataset is read a pickle file on disk and the functions are returned from the dataset.
+    2. use_dataset = False: In this mode, the dataset is not used and the functions are returned as placeholders. The list of functions is read from the disk using `ls`.
+
+    :param dataset_path: path to the dataset
+    :param path_to_save_dataset: path to save the dataset
+    :param dataset_format: format of the dataset (PICKLE, JSON, BZ2)
+    :param use_dataset: whether to use the dataset or not
+    :param shuffle: whether to shuffle the dataset or not
+    :param seed: seed for the random number generator
+    """
+
     def __init__(
         self,
-        dataset_path,
-        path_to_save_dataset,
-        dataset_format,
+        dataset_path: str,
+        path_to_save_dataset: str,
+        dataset_format: DataSetFormat.BZ2 | DataSetFormat.JSON | DataSetFormat.PICKLE,
         use_dataset=False,
         shuffle=False,
+        seed=None,
     ):
         self.dataset_path = dataset_path
         self.path_to_save_dataset = path_to_save_dataset
@@ -36,6 +57,7 @@ class DatasetAgent:
         self.dataset_name = dataset_path.split("/")[-1].split(".")[0]
         self.current_function = 0
         self.dataset_size = 0
+        self.seed = seed
 
         if use_dataset:
             print(f"reading dataset from json at:{dataset_path}")
@@ -60,14 +82,20 @@ class DatasetAgent:
             print(f"reading data from ls at: {dataset_path}")
             self.function_names = os.listdir(dataset_path)
 
+        # Shuffle the dataset (can be used with random sampling turned off to get a random order)
         if self.shuffle:
+            # Set the seed if specified (for reproducibility)
+            if self.seed is not None:
+                random.seed(self.seed)
             random.shuffle(self.function_names)
 
         self.dataset_size = len(self.function_names)
 
-    def get_next_function(self, random=False):
+    def get_next_function(self, random=False) -> Tuple[str, dict]:
+        # Choose a random function
         if random:
             function_name = np.random.choice(self.function_names)
+        # Choose the next function sequentially
         else:
             function_name = self.function_names[
                 self.current_function % self.dataset_size
@@ -77,10 +105,12 @@ class DatasetAgent:
         print(
             f"Selected function with index: {self.current_function}, name: {function_name}"
         )
-        print(self.function_names[:10])
 
+        # If we are using the dataset, return the function from the dataset
         if self.use_dataset:
             return function_name, self.dataset[function_name]
+
+        # If we are not using the dataset, return placeholders
         else:
             return function_name, {
                 "program_annotation": None,
@@ -88,17 +118,31 @@ class DatasetAgent:
                 "schedules_solver_results_dict": {},
             }
 
-    def update_dataset(self, function_name, function_dict):
+    # Update the dataset with the new function
+    def update_dataset(self, function_name: str, function_dict: dict) -> bool:
+        """
+        Update the dataset with the new function
+        :param function_name: name of the function
+        :param function_dict: dictionary containing the function information
+        :return: True if the dataset was saved successfully
+        """
         self.dataset[function_name] = function_dict
         self.nbr_updates += 1
         print(f"# updates: {self.nbr_updates}")
         if self.nbr_updates % SAVING_FREQUENCY == 0:
             if self.nbr_updates % (2 * SAVING_FREQUENCY):
-                self.save_dataset_to_disk(version=2)
+                return self.save_dataset_to_disk(version=2)
             else:
-                self.save_dataset_to_disk(version=1)
+                return self.save_dataset_to_disk(version=1)
+        return False
 
-    def save_dataset_to_disk(self, version=1):
+    # Save the dataset to disk
+    def save_dataset_to_disk(self, version=1) -> bool:
+        """
+        Save the dataset to disk
+        :param version: version of the dataset to save (1 or 2)
+        :return: True if the dataset was saved successfully
+        """
         print("[Start] Save the legality_annotations_dict to disk")
 
         updated_dataset_name = (
--- utils/rl_autoscheduler_config.py
+++ utils/rl_autoscheduler_config.py
@@ -43,15 +43,15 @@ class TiramisuConfig:
     env_type: Literal["model", "cpu"] = "cpu"
     model_checkpoint: str = "/data/scratch/hbenyamina/model_published_nn_finale.pt"
     compile_tiramisu_cmd: str = 'printf "Compiling ${FILE_PATH}\n" >> ${FUNC_DIR}log.txt;\
-        ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include  -Wl,--no-as-needed -ldl -g -fno-rtti   -lpthread -std=c++17 -O0 -o ${FILE_PATH}.o -c ${FILE_PATH};\
-        ${CXX} -Wl,--no-as-needed -ldl -g -fno-rtti   -lpthread -std=c++17 -O0 ${FILE_PATH}.o -o ./${FILE_PATH}.out   -L${TIRAMISU_ROOT}/build  -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64  -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib  -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl'
+        ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include  -Wl,--no-as-needed -ldl -g -fno-rtti   -lpthread -std=c++11 -O0 -o ${FILE_PATH}.o -c ${FILE_PATH};\
+        ${CXX} -Wl,--no-as-needed -ldl -g -fno-rtti   -lpthread -std=c++11 -O0 ${FILE_PATH}.o -o ./${FILE_PATH}.out   -L${TIRAMISU_ROOT}/build  -L${TIRAMISU_ROOT}/3rdParty/Halide/lib  -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib  -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/lib:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl'
 
     run_tiramisu_cmd: str = 'printf "Running ${FILE_PATH}.out\n">> ${FUNC_DIR}log.txt;\
         ./${FILE_PATH}.out>> ${FUNC_DIR}log.txt;'
 
     compile_wrapper_cmd = "cd ${FUNC_DIR};\
             ${GXX} -shared -o ${FUNC_NAME}.o.so ${FUNC_NAME}.o;\
-            ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -std=c++17 -O3 -o ${FUNC_NAME}_wrapper ${FUNC_NAME}_wrapper.cpp ./${FUNC_NAME}.o.so -L${TIRAMISU_ROOT}/build  -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64  -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib  -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl"
+            ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -std=c++11 -O3 -o ${FUNC_NAME}_wrapper ${FUNC_NAME}_wrapper.cpp ./${FUNC_NAME}.o.so -L${TIRAMISU_ROOT}/build  -L${TIRAMISU_ROOT}/3rdParty/Halide/lib  -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib  -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/lib:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl"
 
 
 @dataclass
